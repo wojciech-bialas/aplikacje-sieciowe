d1288e0dc5f94d31d5f6ce2d41ab12d4695db7d1
Wojciech Bialas <wojciech.bialas@protonmail.com>
2020-11-22 20:18:22 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <title>Network Applications</title>
+
+    <meta name="author" content="Wojciech Bialas">
+    <meta name="description" content="PHP application made by Wojciech Bialas for Network Applications faculty">
+    <meta name="keywords" content="network, app, faculty, silesian university">
+
+    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=font1|font2|etc" type="text/css">
+    <link rel="stylesheet" href="stylesheet.css" type="text/css">
+</head>
+<body>
+
+<h1>Main Page</h1>
+
+<p>Welcome to the main page of project for Network Applications faculty at Silesian University</p>
+
+</body>
+</html>
